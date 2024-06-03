8414b67b63933a7dd78e0893549b33fa4a8845ca
caueeugenio <caue_eugenio@hotmail.com>
2024-06-03 00:34:16 -0300

    Creating register view and adding ascending filter

--- index.ts
+++ index.ts
@@ -4,9 +4,10 @@ import express from "express";
 
 const prisma = new PrismaClient();
 const app = express();
-
+const bodyParser = require("body-parser");
 app.use(express.json()); // pra garantir que nossa apliacacao vai resceber o tipo de dados json
 app.set("view engine", "pug");
+app.use(bodyParser.urlencoded({ extended: true }));
 type StudentDTO = {
   name: string;
   email: string;
@@ -14,21 +15,32 @@ type StudentDTO = {
 };
 let allStudents: any;
 app.get("/", async (req, res) => {
-  allStudents = await prisma.student.findMany();
-  res.render("students-view", { allStudents });
-//   res.send(allStudents);
-});
-
-app.post("/", async (req, res) => {
-  const data: StudentDTO = req.body;
-  const student = await prisma.student.create({
-    data: {
-      name: data.name,
-      email: data.email,
-      grade: data.grade,
+  allStudents = await prisma.student.findMany({
+    orderBy: {
+      name: "asc",
     },
   });
-  res.send(student);
+  res.render("students-view", { allStudents });
+});
+
+app.get("/register", async (req, res) => {
+  res.render("register-student-view", {});
+});
+app.post("/register", async (req, res) => {
+  const data: StudentDTO = req.body;
+  try {
+    await prisma.student.create({
+      data: {
+        name: data.name,
+        email: data.email,
+        grade: data.grade,
+      },
+    });
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).send("Erro ao realizar cadastro.");
+    res.redirect("/");
+  }
 });
 
 app.listen(3000, () => {
